a48ccbbbc667343a1daf66cd7aa89599d371e5b4
Taiki Yamada <yamada@microweb.jp>
2021-05-30 04:31:01 +0900

    jissou

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,69 @@
 <template>
-  <div class="container">
+  <section class="container">
+    <h1>Todo App</h1>
+    <p>
+      <input type="text" name="content" v-model="content" @focus="set_flg" />
+    </p>
     <div>
-      <Logo />
-      <h1 class="title">vuex_todo_app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <button @click="insert">save</button>
+      <button @click="find">find</button>
     </div>
-  </div>
+    <ul>
+      <li v-for="(todo, index) in display_todos" :key="index">
+        <span>{{ todo.content }}</span>
+        <span>({{ todo.created }})</span>
+        <span @click="remove(todo)">x</span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      content: '',
+      find_flg: false,
+    }
+  },
+  computed: {
+    ...mapState(['todos']),
+    display_todos() {
+      if (this.find_flg) {
+        const arr = []
+        const data = this.todos
+        data.forEach((element) => {
+          if (element.content.toLowerCase() === this.content.toLowerCase()) {
+            arr.push(element)
+          }
+        })
+        return arr
+      } else {
+        return this.todos
+      }
+    },
+  },
+  methods: {
+    insert() {
+      if (this.content === '') return
+
+      this.$store.commit('insert', { content: this.content })
+      this.content = ''
+    },
+    remove(todo) {
+      this.$store.commit('remove', { ...todo })
+    },
+    find() {
+      this.find_flg = true
+    },
+    set_flg() {
+      if (this.find_flg) {
+        this.find_flg = false
+        this.content = ''
+      }
+    },
+  },
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
